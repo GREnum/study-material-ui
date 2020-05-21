f851d3f5ee862d83b0ee38e90c296f089fe91b56
GREnum <111282967+GREnum@users.noreply.github.com>
2020-05-21 07:55:17 +0000

    Fix mistake

--- backend/src/common/common.module.ts
+++ backend/src/common/common.module.ts
@@ -1,3 +1,12 @@
 import { SettingService } from "../modules/setting/setting.service";
 
-import { SettingModule } from "../modules/
+import { SettingModule } from "../modules/setting/setting.module";
+import { Module, SingleScope } from "@nestjs/common";
+import { CommonService } from "./common.service";
+
+import { DatabaseModule } from "../modules/database/database.module";
+
+
+@Module({
+    modules: [SettingModule],
+    compon
