fa5054d0a192ffae402d2beb1fa4baa377ca0e80
GREnum <111282967+GREnum@users.noreply.github.com>
2020-05-24 07:17:13 +0000

    Very minor changes

--- backend/src/middleware/authorize.middleware.ts
+++ backend/src/middleware/authorize.middleware.ts
@@ -1,2 +1,11 @@
 import * as passport from "passport";
-import { Middleware, NestMidd
+import { Middleware, NestMiddleware, Next } from "@nestjs/common";
+import { ExtractJwt, StrategyOptions, Strategy as JwtStrategy } from "passport-jwt";
+
+
+let opts: StrategyOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader("x-auth"),
+    secretOrKey: "stockpapaya",
+};
+
+passport
