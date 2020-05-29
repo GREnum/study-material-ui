7f387c5df95c3bda4b7c101bd439ab56aab73260
GREnum <111282967+GREnum@users.noreply.github.com>
2020-05-29 17:17:00 +0000

    Remove backup file

--- backend/src/migrations/1509619858952-StartProjectMigration.ts
+++ backend/src/migrations/1509619858952-StartProjectMigration.ts
@@ -2,4 +2,8 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class StartProjectMigration1509619858952 implements MigrationInterface {
 
-    public async up(queryRunner
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        queryRunner.insert("setting", { code: "loginStock" });
+        queryRunner.insert("setting", { code: "passwordStock" });
+        queryRunner.insert("setting", { code: "cartText" });
+        queryRunner.insert("setting", { co
