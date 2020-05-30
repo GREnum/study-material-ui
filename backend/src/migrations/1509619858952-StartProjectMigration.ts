e3f7914d0bf0b3cfb92a4114a21aa8bbc1355bea
GREnum <111282967+GREnum@users.noreply.github.com>
2020-05-30 07:08:19 +0000

    Refinements.

--- backend/src/migrations/1509619858952-StartProjectMigration.ts
+++ backend/src/migrations/1509619858952-StartProjectMigration.ts
@@ -6,4 +6,9 @@ export class StartProjectMigration1509619858952 implements MigrationInterface {
         queryRunner.insert("setting", { code: "loginStock" });
         queryRunner.insert("setting", { code: "passwordStock" });
         queryRunner.insert("setting", { code: "cartText" });
-        queryRunner.insert("setting", { co
+        queryRunner.insert("setting", { code: "productText" });
+        queryRunner.insert("setting", { code: "orderComment" });
+        queryRunner.insert("user", { name: "admin", password: "74913f5cd5f61ec0bcfdb775414c2fb3d161b620", isAdmin: true });
+    }
+
+    public async down(queryRunner: QueryRunner): Promis
