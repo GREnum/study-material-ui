9cb08357972b5251cdea862d308f306cd42af328
GREnum <111282967+GREnum@users.noreply.github.com>
2020-06-01 17:35:58 +0000

    First check-in

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -3,4 +3,9 @@ import { Module } from "@nestjs/common";
 // import { AuthModule } from "../auth/auth.module";
 // import { RoleModule } from "../roles/role.module";
 import { UserModule } from "../user/user.module";
-import { OrderModule } from "../or
+import { OrderModule } from "../order/order.module";
+import { ProductModule } from "../product/product.module";
+import { SettingModule } from "../setting/setting.module";
+import { CommonModule } from "../../common/common.module";
+import { CounterpartyModule } from "../counterparty/counterparty.module";
+// import { AuthServ
