971820a514d149fb0be0489c7705f391ed6111e9
GREnum <111282967+GREnum@users.noreply.github.com>
2020-06-05 16:01:50 +0000

    Update from Forestry.io - about.md

--- backend/src/modules/database/dev.database.config.ts
+++ backend/src/modules/database/dev.database.config.ts
@@ -1,3 +1,13 @@
 import * as path from "path";
 import { Component } from "@nestjs/common";
-import { ConnectionO
+import { ConnectionOptions } from "typeorm";
+
+import { User } from "../user/user.entity";
+import { Setting } from "../setting/setting.entity";
+import { DatabaseConfig } from "./database.config";
+
+
+@Component()
+export class DevDatabaseConfig extends DatabaseConfig {
+    public getConfiguration(): ConnectionOptions {
+        consol
