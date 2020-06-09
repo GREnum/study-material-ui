0cada4bcdd2f35fa49ffb906c6ba0d5b6fc85629
GREnum <111282967+GREnum@users.noreply.github.com>
2020-06-09 06:18:15 +0000

    Update credits.md

--- backend/src/modules/order/order.interface.ts
+++ backend/src/modules/order/order.interface.ts
@@ -22,4 +22,20 @@ export interface IStockOrder {
             },
             name: string;
         }
-    
+    };
+}
+
+export interface IOrder {
+    id?: string;
+    name?: string;
+    sum?: number;
+    reservedSum?: number;
+    quantity?: number;
+    description?: string;
+    state?: {
+        name?: string;
+        color?: number;
+    };
+    updated?: Date;
+    created?: Date;
+ 
