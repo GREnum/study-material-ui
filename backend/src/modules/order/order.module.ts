1ba9e19e2e73d312fda12aba7e6a90abd0606648
GREnum <111282967+GREnum@users.noreply.github.com>
2020-06-12 19:58:43 +0000

    foundation

--- backend/src/modules/order/order.module.ts
+++ backend/src/modules/order/order.module.ts
@@ -6,4 +6,13 @@ import { LoggingMiddleware } from "../../middleware/logging.middleware";
 import { AuthorizeMiddleware } from "../../middleware/authorize.middleware";
 import { SettingService } from "../setting/setting.service";
 import { SettingModule } from "../setting/setting.module";
-import { Comm
+import { CommonService } from "../../common/common.service";
+import { CommonModule } from "../../common/common.module";
+import { UserModule } from "../user/user.module";
+import { UserService } from "../user/user.service";
+
+
+@Module({
+    modules: [
+        UserModule,
+        
