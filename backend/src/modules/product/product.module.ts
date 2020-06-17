b9a72bc655ba1b8e43221822cfda03e62e147b33
GREnum <111282967+GREnum@users.noreply.github.com>
2020-06-17 16:06:57 +0000

    remove package-lock.json

--- backend/src/modules/product/product.module.ts
+++ backend/src/modules/product/product.module.ts
@@ -1,3 +1,7 @@
 import { MiddlewaresConsumer, Module } from "@nestjs/common";
 
-import { CommonModule } from "../../common/comm
+import { CommonModule } from "../../common/common.module";
+import { CommonService } from "../../common/common.service";
+import { ProductService } from "./product.service";
+import { ProductController } from "./product.controller";
+import { LoggingMiddleware } from "../../middleware/logging.middleware
