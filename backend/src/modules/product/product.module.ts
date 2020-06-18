eb400ad0c2f8804c3bd8894b4e9c8038f9d006f0
GREnum <111282967+GREnum@users.noreply.github.com>
2020-06-18 20:00:38 +0000

    [maven-release-plugin] prepare release 1.0

--- backend/src/modules/product/product.module.ts
+++ backend/src/modules/product/product.module.ts
@@ -4,4 +4,9 @@ import { CommonModule } from "../../common/common.module";
 import { CommonService } from "../../common/common.service";
 import { ProductService } from "./product.service";
 import { ProductController } from "./product.controller";
-import { LoggingMiddleware } from "../../middleware/logging.middleware
+import { LoggingMiddleware } from "../../middleware/logging.middleware";
+import { AuthorizeMiddleware } from "../../middleware/authorize.middleware";
+import { SettingModule } from "../setting/setting.module";
+import { SettingService } from "../setting/setting.service";
+import { UserService } from "../user/user.service";
+import { User
