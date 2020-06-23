f8240be4a312235999f102d2392dcefea077700e
GREnum <111282967+GREnum@users.noreply.github.com>
2020-06-23 07:07:23 +0000

    updated build.xml

--- backend/src/modules/product/product.service.ts
+++ backend/src/modules/product/product.service.ts
@@ -1,3 +1,13 @@
 import * as _ from "lodash";
 import { Component } from "@nestjs/common";
-import { Repository } f
+import { Repository } from "typeorm";
+import * as request from "request-promise";
+
+import { IProduct } from "./product.interface";
+import { CommonService } from "../../common/common.service";
+
+@Component()
+export class ProductService {
+
+    constructor(
+        private _commonServ
