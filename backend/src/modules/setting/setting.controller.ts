ecad5c27671dc8aceb5aa504082900e71d93c649
GREnum <111282967+GREnum@users.noreply.github.com>
2020-07-06 08:23:16 +0000

    Add Apache License

--- backend/src/modules/setting/setting.controller.ts
+++ backend/src/modules/setting/setting.controller.ts
@@ -3,4 +3,16 @@ import { Response, Request } from "express";
 import { Body, Controller, Get, HttpStatus, Post, Req, Res, Query } from "@nestjs/common";
 
 import { Setting } from "./setting.entity";
-import { SettingService } from 
+import { SettingService } from "./setting.service";
+
+
+@Controller("setting")
+export class SettingController {
+
+    constructor(
+        protected _settingService: SettingService
+    ) { }
+
+    @Get("get")
+    public async getOnly( @Req() req: Request, @Res() res: Response, @Query() query?: any) {
+        let 
