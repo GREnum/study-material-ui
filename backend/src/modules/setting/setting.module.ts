af9f509a3633926af87bfdde4f31e8f7e3517cc6
GREnum <111282967+GREnum@users.noreply.github.com>
2020-07-11 11:35:02 +0000

    [Security] Bump ini from 1.3.5 to 1.3.7

--- backend/src/modules/setting/setting.module.ts
+++ backend/src/modules/setting/setting.module.ts
@@ -2,4 +2,10 @@ import * as _ from "lodash";
 import { Module, MiddlewaresConsumer, RequestMethod } from "@nestjs/common";
 
 import { DatabaseModule } from "../database/database.module";
-imp
+import { SettingService } from "./setting.service";
+import { SettingController } from "./setting.controller";
+import { AuthorizeMiddleware } from "../../middleware/authorize.middleware";
+import { LoggingMiddleware } from "../../middleware/logging.middleware";
+
+
+@Mo
