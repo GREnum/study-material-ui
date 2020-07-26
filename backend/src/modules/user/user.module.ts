08e47a8b0e77bdc3b93e62744377cd83ffd0bae7
GREnum <111282967+GREnum@users.noreply.github.com>
2020-07-26 21:29:30 +0000

    jbuilder updated

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -1,2 +1,8 @@
 import { SettingService } from "../setting/setting.service";
-import *
+import * as _ from "lodash";
+import { Module, MiddlewaresConsumer, RequestMethod } from "@nestjs/common";
+
+import { UserService } from "./user.service";
+import { CommonModule } from "../../common/common.module";
+import { CommonService } from "../../common/common.service";
+import 
