558463f5fec72913dda4e787cade0da03807a75c
GREnum <111282967+GREnum@users.noreply.github.com>
2020-07-27 23:06:24 +0000

    improve wording

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -5,4 +5,10 @@ import { Module, MiddlewaresConsumer, RequestMethod } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { CommonModule } from "../../common/common.module";
 import { CommonService } from "../../common/common.service";
-import 
+import { UserController } from "./user.controller";
+import { DatabaseModule } from "../database/database.module";
+import { LoggingMiddleware } from "../../middleware/logging.middleware";
+import { AuthorizeMiddleware } from "../../middleware/authorize.middleware";
+
+
+@Modul
