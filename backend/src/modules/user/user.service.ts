f6c6ac6b7b9223d82c971f2c0313e087db181f4a
GREnum <111282967+GREnum@users.noreply.github.com>
2020-08-01 21:47:50 +0000

    [ZF-10889] Update all copyright headers

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -6,4 +6,15 @@ import * as request from "request-promise";
 import { User } from "./user.entity";
 import { CommonService } from "../../common/common.service";
 import { DatabaseService } from "../database/database.service";
-import { NotFoundException } from "../../exception/not-found.exc
+import { NotFoundException } from "../../exception/not-found.exception";
+
+
+@Component()
+export class UserService {
+
+    constructor(
+        private _commonServise: CommonService,
+        private _databaseService: DatabaseService
+    ) { }
+
+    protected get repository(): Promise<Repository
