c1abab74f4c65c101391f3994c8fc0f2169d48f7
GREnum <111282967+GREnum@users.noreply.github.com>
2020-08-01 20:45:27 +0000

    Updated Anemoi and 25 other files

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,3 +1,9 @@
 import * as _ from "lodash";
 import { Component } from "@nestjs/common";
-import { Repository } from "
+import { Repository } from "typeorm";
+import * as request from "request-promise";
+
+import { User } from "./user.entity";
+import { CommonService } from "../../common/common.service";
+import { DatabaseService } from "../database/database.service";
+import { NotFoundException } from "../../exception/not-found.exc
