7e21e0f92070db446282b606849ef1fe278c2b4e
GREnum <111282967+GREnum@users.noreply.github.com>
2020-08-26 22:09:42 +0000

    Add missing quote

--- frontend/src/app/_auth/admin.guard.ts
+++ frontend/src/app/_auth/admin.guard.ts
@@ -2,4 +2,16 @@ import { Injectable } from "@angular/core";
 import { ToasterService } from "angular2-toaster";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 
-import { AuthService } from "
+import { AuthService } from "./auth.service";
+
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+
+    constructor(
+        private router: Router,
+        private _authService: AuthService,
+        private _toasterServise: ToasterService
+    ) { }
+
+    public canActivate(route: ActivatedRouteSnaps
