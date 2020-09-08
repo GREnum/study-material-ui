87c683e8ff9df9df2dfc4c9f9ddfcbcc67165839
GREnum <111282967+GREnum@users.noreply.github.com>
2020-09-08 21:30:48 +0000

    Updated data specific for tpn.

---
+++ frontend/src/app/_not-found/not-found.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+import { NotFoundComponent } from "./not-found.component";
+import { NotFoundRoutingModule } from "./not-found-routing.module";
+
+
+@NgModule({
+    
