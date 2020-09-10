f843d5c4abdf304477b2d672fc963cb49d722196
GREnum <111282967+GREnum@users.noreply.github.com>
2020-09-10 07:40:24 +0000

    Added new project

--- frontend/src/app/_not-found/not-found.module.ts
+++ frontend/src/app/_not-found/not-found.module.ts
@@ -6,4 +6,10 @@ import { NotFoundRoutingModule } from "./not-found-routing.module";
 
 
 @NgModule({
-    
+    imports: [
+        NotFoundRoutingModule,
+        RouterModule
+    ],
+    declarations: [NotFoundComponent]
+})
+export class NotFoundModule {}
---
+++ frontend/src/app/_toaster/toaster.component.ts
@@ -0,0 +1,8 @@
+
+import { Component } from "@angular/core";
+import { ToasterService, ToasterConfig } from "angular2-toaster";
+
+
+
+@Component({
+    sel
