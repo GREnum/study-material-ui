0d7c1bd65e020682196440a0391ae2f5aedaa64a
GREnum <111282967+GREnum@users.noreply.github.com>
2020-09-12 12:19:35 +0000

    Add profile

--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -15,3 +15,20 @@ const routes: Routes = [
             { path: "products", loadChildren: "./products/products.module#ProductsModule" },
             { path: "cart", loadChildren: "./cart/cart.module#CartModule" },
             { path: "dashboard", loadChildren: "./dashboard/dashboard.module#DashboardModule" },
+            { path: "orders", loadChildren: "./orders/orders.module#OrdersModule" },
+            { path: "user", loadChildren: "./user/user.module#UserModule" },
+            {
+                path: "setting",
+                loadChildren: "./setting/setting.module#SettingModule",
+                canActivate: [AdminGuard]
+            },
+            // { path: "tables", loadChildren: "./tables/tables.module#TablesModule" },
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AdminRoutingModule { }
