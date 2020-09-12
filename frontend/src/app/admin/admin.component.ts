b00e58ccc7fff68394d277ed9b0c7898a02bc5e1
GREnum <111282967+GREnum@users.noreply.github.com>
2020-09-12 16:49:00 +0000

    Text updated

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,2 +1,14 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@a
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { AuthService } from "../_auth/auth.service";
+
+
+@Component({
+    selector: "app-admin",
+    templateUrl: "./admin.component.html",
+    styleUrls: ["./admin.component.scss"]
+})
+export class AdminComponent implements OnInit {
+
+    construc
