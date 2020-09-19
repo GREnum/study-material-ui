7a95102237b730a4b17e17e969f4a8add854f915
GREnum <111282967+GREnum@users.noreply.github.com>
2020-09-19 08:32:04 +0000

    [INTERNAL][TEST] sap.uxap ObjectPageAnchorBar: reference images changed

--- frontend/src/app/admin/cart/cart-resolver-text.service.ts
+++ frontend/src/app/admin/cart/cart-resolver-text.service.ts
@@ -3,4 +3,14 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
 
 import { ISetting } from "../setting/setting.interface";
-import { SettingService } from "../setting/setting.ser
+import { SettingService } from "../setting/setting.service";
+
+
+@Injectable()
+export class CartTextResolverService implements Resolve<ISetting> {
+
+    constructor(
+        private _settingService: SettingService
+    ) { }
+
+    public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Ob
