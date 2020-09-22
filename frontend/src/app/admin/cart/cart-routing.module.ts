42c57f3adcba8d435120d60040320673fbb06fe3
GREnum <111282967+GREnum@users.noreply.github.com>
2020-09-22 16:57:46 +0000

    updated filters

--- frontend/src/app/admin/cart/cart-routing.module.ts
+++ frontend/src/app/admin/cart/cart-routing.module.ts
@@ -1,4 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import
+import { CartComponent } from "./cart.component";
+import { CartResolverService } from "./cart-resolver.service";
+import { AllUsersResolverService } from "../user/all-users-resolve.service";
+import { CartTextResolverService } from "./cart-resolver-text.service";
+
+
+
+const routes: Route
