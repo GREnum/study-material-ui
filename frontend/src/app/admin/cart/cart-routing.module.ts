b152f53c157eee9d67ad97ebba222fe9d018edcf
GREnum <111282967+GREnum@users.noreply.github.com>
2020-09-25 22:38:19 +0000

    Fix require paths

--- frontend/src/app/admin/cart/cart-routing.module.ts
+++ frontend/src/app/admin/cart/cart-routing.module.ts
@@ -21,4 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(rou
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class CartRoutingModule { }
