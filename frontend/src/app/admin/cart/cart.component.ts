fbd41b46b6fb6c951c716e7eda56f04e9f8d1f34
GREnum <111282967+GREnum@users.noreply.github.com>
2020-10-24 11:26:07 +0000

    Update VM.md

--- frontend/src/app/admin/cart/cart.component.ts
+++ frontend/src/app/admin/cart/cart.component.ts
@@ -4,4 +4,11 @@ import { Observable } from "rxjs/Rx";
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 
-import { OrdersService
+import { OrdersService } from "../orders/orders.service";
+import { IProduct, PositionsService } from "../components/positions/position.service";
+import { RegisterUser } from "../user/user.component";
+import { AuthService } from "../../_auth/auth.service";
+
+
+@Component({
+    selec
