65ddc63a0214073135ef5eb6338fb032b50b38fc
GREnum <111282967+GREnum@users.noreply.github.com>
2020-11-02 16:19:01 +0000

    clean

--- frontend/src/app/admin/cart/cart.module.ts
+++ frontend/src/app/admin/cart/cart.module.ts
@@ -2,4 +2,8 @@ import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
-import { CartCom
+import { CartComponent } from "./cart.component";
+import { OrdersService } from "../orders/orders.service";
+import { PositionsModule } from "../components/positions/positions.module";
+import { CartRoutingModule } from "./cart-routing.module";
+import { CartRes
