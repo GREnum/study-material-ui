877061c33ba7547ad5631eab1938def800761cfd
GREnum <111282967+GREnum@users.noreply.github.com>
2020-11-03 09:49:59 +0000

    Reviewed and landed by Darin.

--- frontend/src/app/admin/cart/cart.module.ts
+++ frontend/src/app/admin/cart/cart.module.ts
@@ -6,4 +6,13 @@ import { CartComponent } from "./cart.component";
 import { OrdersService } from "../orders/orders.service";
 import { PositionsModule } from "../components/positions/positions.module";
 import { CartRoutingModule } from "./cart-routing.module";
-import { CartRes
+import { CartResolverService } from "./cart-resolver.service";
+import { AllUsersResolverService } from "../user/all-users-resolve.service";
+import { CartTextResolverService } from "./cart-resolver-text.service";
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        CartRoutingModule,
+ 
