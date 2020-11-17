61ed3c5ae15fbafd1616757e744b654f07c57c93
GREnum <111282967+GREnum@users.noreply.github.com>
2020-11-17 11:35:49 +0000

    jffs2: Fix page lock / f->sem deadlock

--- frontend/src/app/admin/components/header/header.component.ts
+++ frontend/src/app/admin/components/header/header.component.ts
@@ -5,4 +5,14 @@ import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
     selector: "app-header",
-    templateUrl: "./header.
+    templateUrl: "./header.component.html",
+    styleUrls: ["./header.component.scss"]
+})
+export class HeaderComponent implements OnInit {
+    public userName: string;
+    public pushRightClass: string = "push-right";
+    constructor(
+        private _authService: AuthService
+    ) { }
+
+    public ng
