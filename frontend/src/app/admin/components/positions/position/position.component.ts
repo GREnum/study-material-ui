aec92a38c5abe906de55b062f9e69884b81e3772
GREnum <111282967+GREnum@users.noreply.github.com>
2020-11-24 14:14:05 +0000

    updated stylesheet

--- frontend/src/app/admin/components/positions/position/position.component.ts
+++ frontend/src/app/admin/components/positions/position/position.component.ts
@@ -1,1 +1,9 @@
-im
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+
+@Component({
+    selector: "position-comp",
+    templateUrl: "./position.component.html",
+    styleUrls: ["./position.component.scss"]
+})
+export class PositionComponent {
+    @Input() public pos
