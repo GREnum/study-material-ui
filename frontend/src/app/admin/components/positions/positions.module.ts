3088597a5865594d06ce0a8fc236975d580a6c97
GREnum <111282967+GREnum@users.noreply.github.com>
2020-11-28 10:53:27 +0000

    Update German translations

--- frontend/src/app/admin/components/positions/positions.module.ts
+++ frontend/src/app/admin/components/positions/positions.module.ts
@@ -1,1 +1,10 @@
-import {
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+
+import { PositionComponent } from "./position/position.component";
+import { PositionsComponent } from "./positions.component";
+
+
+@NgModule({
+ 
