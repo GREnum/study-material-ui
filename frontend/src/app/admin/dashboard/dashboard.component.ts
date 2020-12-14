10b586d3dfb8566167698a9bf8739be516fde148
GREnum <111282967+GREnum@users.noreply.github.com>
2020-12-14 08:59:59 +0000

    Simplify error handling

--- frontend/src/app/admin/dashboard/dashboard.component.ts
+++ frontend/src/app/admin/dashboard/dashboard.component.ts
@@ -1,3 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 
-import { DashboardService } from "./das
+import { DashboardService } from "./dashboard.service";
+
+
+@Component({
+    selector: "app-dashboard",
+    templateUrl: "./dashboard.component.html"
+})
+export class DashboardComponent implements OnInit {
+    public counterparties: any;
+    constructor(
+        private _
