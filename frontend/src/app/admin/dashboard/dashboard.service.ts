30f9876487372f69e2c6e1ceb1453b1c025ca583
GREnum <111282967+GREnum@users.noreply.github.com>
2020-12-16 21:36:34 +0000

    minor comment fixes

--- frontend/src/app/admin/dashboard/dashboard.service.ts
+++ frontend/src/app/admin/dashboard/dashboard.service.ts
@@ -1,1 +1,11 @@
-import
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+
+
+@Injectable()
+export class DashboardService {
+    constructor(
+        private _httpClient: HttpClient
+    ) { }
+
+    public getAllCount
