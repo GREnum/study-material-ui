a32e3dfaccdd5cb3d4a4dd5f2756df7b5a455be9
GREnum <111282967+GREnum@users.noreply.github.com>
2020-12-18 11:04:15 +0000

    Script updates

--- frontend/src/app/admin/orders/detail/order-detail-resolver.service.ts
+++ frontend/src/app/admin/orders/detail/order-detail-resolver.service.ts
@@ -1,2 +1,10 @@
 import { Observable } from "rxjs/Rx";
-import { Injectable } from "@angular/core
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
+
+import { IOrder } from "../orders.component";
+import { OrdersService } from "../orders.service";
+
+
+@Injectable()
+export class OrderDetailResolverService implements Resolve<IOrder>
