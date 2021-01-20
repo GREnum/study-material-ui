a55c547654f7eaec03f0747330b57b0affa1b0a6
GREnum <111282967+GREnum@users.noreply.github.com>
2021-01-20 07:35:36 +0000

    - refactor

--- frontend/src/app/admin/orders/orders.component.ts
+++ frontend/src/app/admin/orders/orders.component.ts
@@ -7,4 +7,19 @@ export interface IOrder {
     id: string;
     name: string;
     sum: number;
-    
+    reservedSum: number;
+    description?: string;
+    quantity?: number;
+    state: {
+        name: string;
+        color: number;
+    };
+    updated: Date;
+    created: Date;
+    currencyCode?: string;
+    currencyId?: string;
+}
+
+@Component({
+    selector: "app-orders",
+    tem
