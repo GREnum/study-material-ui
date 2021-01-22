b3a9cb9d3a3400f61596788771ed777b02a15841
GREnum <111282967+GREnum@users.noreply.github.com>
2021-01-22 09:29:09 +0000

    Remove dead file

--- frontend/src/app/admin/orders/orders.service.ts
+++ frontend/src/app/admin/orders/orders.service.ts
@@ -1,3 +1,16 @@
 import { Observable } from "rxjs/Rx";
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+
+import { IOrder } from "./orders.component";
+import { IProduct } from "../components/positions/position.service";
+
+
+@Injectable()
+export class OrdersService {
+    constructor(
+        private _httpClient: HttpClient
+    ) { }
+
+    public getAllOrders(): Observable<any> {
+        return this._http
