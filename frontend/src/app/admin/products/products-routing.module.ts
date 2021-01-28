a1e4326e6e9ceae29c181a66c30189ca6d4a5c5a
GREnum <111282967+GREnum@users.noreply.github.com>
2021-01-28 21:23:45 +0000

    CV update

--- frontend/src/app/admin/products/products-routing.module.ts
+++ frontend/src/app/admin/products/products-routing.module.ts
@@ -1,2 +1,12 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@ang
+import { Routes, RouterModule } from "@angular/router";
+
+import { ProductsComponent } from "./products.component";
+import { ProductTextResolverService } from "./products-resolver-text.service";
+
+
+const routes: Routes = [
+    {
+        path: "",
+        component: ProductsComponent,
+  
