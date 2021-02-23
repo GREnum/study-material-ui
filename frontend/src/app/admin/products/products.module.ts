60353760dc8e9e396e2afbc6bd99d915303ee29b
GREnum <111282967+GREnum@users.noreply.github.com>
2021-02-23 13:22:03 +0000

    fix Makefile

--- frontend/src/app/admin/products/products.module.ts
+++ frontend/src/app/admin/products/products.module.ts
@@ -5,4 +5,12 @@ import { InfiniteScrollModule } from "ngx-infinite-scroll";
 
 import { ProductsService } from "./products.service";
 import { PositionsModule } from "../components/positions/positions.module";
-import { ProductsComponent } f
+import { ProductsComponent } from "./products.component";
+import { ProductsRoutingModule } from "./products-routing.module";
+import { ProductTextResolverService } from "./products-resolver-text.service";
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+       
