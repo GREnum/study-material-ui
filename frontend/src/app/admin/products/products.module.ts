b75de4d41e374f80a37898a74ddc89cade9d3a10
GREnum <111282967+GREnum@users.noreply.github.com>
2021-02-23 11:07:36 +0000

    Fixed a warning

--- frontend/src/app/admin/products/products.module.ts
+++ frontend/src/app/admin/products/products.module.ts
@@ -1,3 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { F
+import { FormsModule } from "@angular/forms";
+import { InfiniteScrollModule } from "ngx-infinite-scroll";
+
+import { ProductsService } from "./products.service";
+import { PositionsModule } from "../components/positions/positions.module";
+import { ProductsComponent } f
