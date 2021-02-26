c0a975dd8ba5f9aad5fa71d3211e1ee44e48a05c
GREnum <111282967+GREnum@users.noreply.github.com>
2021-02-26 19:31:10 +0000

    correct url

--- frontend/src/app/admin/setting/setting-routing.module.ts
+++ frontend/src/app/admin/setting/setting-routing.module.ts
@@ -2,4 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { SettingComponent } from "./setting.component";
-imp
+import { SettingResolverService } from "./setting-resolve.service";
+
+
+const routes: Routes = [
+    { path: "", component: SettingComponent, resolve: { setting: SettingResolverService } }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [
