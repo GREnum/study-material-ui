3f032e9646476cc4ae1aed3ccb4effdca84972df
GREnum <111282967+GREnum@users.noreply.github.com>
2021-03-17 20:06:35 +0000

    fix #64

--- frontend/src/app/admin/setting/setting.component.ts
+++ frontend/src/app/admin/setting/setting.component.ts
@@ -1,4 +1,13 @@
 import * as _ from "lodash";
 import { NgForm } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
-import { Compo
+import { Component, OnInit } from "@angular/core";
+import { ToasterService } from "angular2-toaster";
+
+import { ISetting } from "./setting.interface";
+import { SettingService } from "./setting.service";
+
+
+@Component({
+    selector: "app-setting",
+    templateUrl: "./setting.component.html"
