f20d1457aba1d22cd5629c2af5e28b03b2d0aa2d
GREnum <111282967+GREnum@users.noreply.github.com>
2021-03-18 10:38:40 +0000

    update variable name

--- frontend/src/app/admin/setting/setting.component.ts
+++ frontend/src/app/admin/setting/setting.component.ts
@@ -11,3 +11,12 @@ import { SettingService } from "./setting.service";
 @Component({
     selector: "app-setting",
     templateUrl: "./setting.component.html"
+})
+export class SettingComponent implements OnInit {
+    public setting: ISetting[];
+    public folders: any[];
+    public loginStock: ISetting;
+    public passwordStock: ISetting;
+    public cartText: ISetting;
+    public productText: ISetting;
+    pu
