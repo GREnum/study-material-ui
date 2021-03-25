e962bae644305961db88bd8122c3a0e62b6c3942
GREnum <111282967+GREnum@users.noreply.github.com>
2021-03-25 17:51:43 +0000

    Adding source code

--- frontend/src/app/admin/user/all-users-resolve.service.ts
+++ frontend/src/app/admin/user/all-users-resolve.service.ts
@@ -1,3 +1,12 @@
 import { Observable } from "rxjs/Rx";
 import { Injectable } from "@angular/core";
-import { ActivatedRout
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot, ActivatedRoute } from "@angular/router";
+
+import { AuthService } from "../../_auth/auth.service";
+
+
+@Injectable()
+export class AllUsersResolverService implements Resolve<any> {
+
+    constructor(
+        private _route: Act
