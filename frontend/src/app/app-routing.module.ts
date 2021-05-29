47fb70c1c801439b251c6101037c238f95901d2c
GREnum <111282967+GREnum@users.noreply.github.com>
2021-05-29 15:32:25 +0000

    sitemap update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,1 +1,9 @@
-import { 
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { AuthGuard } from "./_auth/auth.guard";
+
+
+const routes: Routes = [
+    { path: "", redirectTo: "admin", pathMatch: "full" },
+    { path: "admin", loadChildren: "./admi
