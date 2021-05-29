2780c32172ab857a60af48077366c327781b7101
GREnum <111282967+GREnum@users.noreply.github.com>
2021-05-29 08:11:51 +0000

    more crap

--- frontend/src/app/admin/user/user.service.ts
+++ frontend/src/app/admin/user/user.service.ts
@@ -1,3 +1,16 @@
 import { Observable } from "rxjs/Rx";
 import { Injectable } from "@angular/core";
-import { H
+import { HttpClient} from "@angular/common/http";
+
+
+@Injectable()
+export class UserService {
+    constructor(
+        private _httpClient: HttpClient
+    ) { }
+
+    public getAllCurrency(): Observable<any> {
+        return this._httpClient.get("/api/user/currency");
+    }
+
+}
