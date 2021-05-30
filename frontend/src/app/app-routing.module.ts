14f2d64b79ae0d5d0f40c2de05febefed9af524b
GREnum <111282967+GREnum@users.noreply.github.com>
2021-05-30 09:46:17 +0000

    bumped version after release

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,4 +6,8 @@ import { AuthGuard } from "./_auth/auth.guard";
 
 const routes: Routes = [
     { path: "", redirectTo: "admin", pathMatch: "full" },
-    { path: "admin", loadChildren: "./admi
+    { path: "admin", loadChildren: "./admin/admin.module#AdminModule", canActivate: [AuthGuard] },
+    { path: "login", loadChildren: "./pages/user/login/login.module#LoginModule" },
+    { path: "not-found", loadChildren: "./_not-found/not-found.module#NotFoundModule" },
+    { path: "**", redirectTo: "not-found" }
+];
