5395990ccfa60d594191cccbca751dbf3b277cf7
GREnum <111282967+GREnum@users.noreply.github.com>
2021-05-31 06:20:14 +0000

    \tmodified:   squiff/various

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,4 +3,12 @@ import { Router } from "@angular/router";
 import { ToasterService } from "angular2-toaster";
 
 
-@Compon
+@Component({
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+    constructor(private _toasterServise: ToasterService) { }
+    public popToast() {
+        this._toasterServise.pop(
