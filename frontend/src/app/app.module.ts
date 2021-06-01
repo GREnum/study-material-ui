ee429ca09c9d9d496668b54177defaa34c5b4308
GREnum <111282967+GREnum@users.noreply.github.com>
2021-06-01 16:13:17 +0000

    v1.2.3

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,11 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientMo
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from "@angular/common/http";
+import { ToasterModule } from "angular2-toaster";
+
+
+import { AuthGuard } from "./_auth/auth.guard";
+import { AuthService } from "./_auth/auth.service";
+import { AppComponent } from "./app.component";
+import { Toast
