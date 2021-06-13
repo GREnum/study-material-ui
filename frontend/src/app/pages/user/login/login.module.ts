4773ce077b505aec47bee1a547bc0bc78c800a29
GREnum <111282967+GREnum@users.noreply.github.com>
2021-06-13 08:42:09 +0000

    more binary

--- frontend/src/app/pages/user/login/login.module.ts
+++ frontend/src/app/pages/user/login/login.module.ts
@@ -4,3 +4,16 @@ import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 
 import { LoginComponent } from "./login.component";
+import { LoginRoutingModule } from "./login-routing.module";
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        HttpClientModule,
+        LoginRoutingModule
+    ],
+    declarations: [LoginComponent]
+})
+export class LoginM
